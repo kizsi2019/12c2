b868fef6ae78838459c33e7d7575a18edfd5200a
Kiss Zsigmond <zsigmond.kiss@gmail.com>
2021-04-19 09:09:34 +0200

    hétfői állapot

---
+++ Kiss Zsigmond/2021 Jedlik Online feladatmegoldás/források/3. Weboldalak kódolása és adatbázis-kezelés/2. adatbázis/megoldasok.sql
@@ -0,0 +1,19 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+-- 11. feladat:
+
+
+-- 13. feladat:
+
+
+-- 14. feladat:
+
+
+-- 15. feladat:
+
+ 
+-- 16. feladat:
+
+  
+-- 17. feladat:
+
